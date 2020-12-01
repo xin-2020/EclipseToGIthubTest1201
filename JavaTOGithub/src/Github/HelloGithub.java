ddf99a9e757c4732b6452a445f0e18ab9d98fee6
xin-2020 <61177039+xin-2020@users.noreply.github.com>
2020-12-01 16:58:01 +0800

    Update HelloGithub.java

--- JavaTOGithub/src/Github/HelloGithub.java
+++ JavaTOGithub/src/Github/HelloGithub.java
@@ -5,5 +5,11 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		
 		System.out.println("hello,github");
+		System.out.println("hello,github");
+		int i=0;
+		for(i;i=0;i++){
+		System.out.println("java is best language! ");
+		}
+		
 	}
 }
